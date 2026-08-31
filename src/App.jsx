b7f104a823d1005ea01d3c8aa31be43461059223
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-31 14:19:17 +0600

    Refactor App component to use AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import Movies from "./pages/Movies/Movies";
-import Series from "./pages/Series/Series";
-import Search from "./pages/Search/Search";
-import Details from "./pages/Details/Details";
-import Watch from "./pages/Watch/Watch";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
-import Profile from "./pages/Profile/Profile";
-
-import MainLayout from "./components/layout/MainLayout";
+import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/series" element={<Series />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/watch/:id" element={<Watch />} />
-        <Route path="/profile" element={<Profile />} />
-      </Route>
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <AppRoutes />;
 }
