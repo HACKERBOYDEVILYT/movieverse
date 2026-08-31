4efb1e5be7df9cba90afbd2b84746d1ba6beeeac
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2026-08-31 13:22:19 +0600

    Update and rename h to MainLayout.jsx

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function MainLayout() {
+  return (
+    <div className="app-shell">
+      <Navbar />
+
+      <main className="main-content">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
